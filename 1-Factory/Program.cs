0f36e71e848c88dad3a1d6c6db98eb0dc6cb07ba
Daniel de Oliveira <iel_182@hotmail.com>
2018-12-27 22:27:19 -0200

    Add Factory pattern

---
+++ 1-Factory/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace _1_Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionFactory = new ConnectionFactory();
+            var sqlConnection = connectionFactory.Initialize(EConnection.SQL);
+            sqlConnection.Connect();
+
+            var oracleConnection = connectionFactory.Initialize(EConnection.Oracle);
+            oracleConnection.Connect();
+        }
+    }
+
+    class ConnectionFactory
+    {
+        public IConnection Initialize(EConnection con)
+        {
+
+            switch (con)
+            {
+                case EConnection.Oracle:
+                    return new Oracle();
+                case EConnection.SQL:
+                    return new SQL();
+                default:
+                    throw new Exception("Connection not found");
+            }
+        }
+    }
+
+    class SQL : IConnection
+    {
+        public void Connect()
+        {
+            Console.WriteLine("SQL Connected");
+        }
+    }
+
+    class Oracle : IConnection
+    {
+        public void Connect()
+        {
+            Console.WriteLine("Oracle Connected");
+        }
+    }
+
+    internal interface IConnection
+    {
+        void Connect();
+    }
+
+    public enum EConnection
+    {
+        Oracle,
+        SQL
+    }
+}
