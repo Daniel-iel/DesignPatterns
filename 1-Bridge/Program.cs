e43d3939b71e42091a933785d48ce116b1afdfa8
Daniel de Oliveira <iel_182@hotmail.com>
2019-02-09 22:00:50 -0200

    Add Bridge Pattern

---
+++ 1-Bridge/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _1_Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Message habbitMQMessage = new HabbitMQMessage();
+            Qeue habbitMQeue = new Qeue(habbitMQMessage);
+            habbitMQeue.SendMessage();
+
+            Message azureServiceBusMessage = new AzureServiceBusMessage();
+            Qeue azureServiceBusQeue = new Qeue(azureServiceBusMessage);
+            azureServiceBusQeue.SendMessage();
+        }
+    }
+
+    abstract class Message
+    {
+        public abstract void SendMessage();
+    }
+
+    class Qeue
+    {
+        private readonly Message message;
+
+        public Qeue(Message message)
+        {
+            this.message = message;
+        }
+
+        public void SendMessage()
+        {
+            message.SendMessage();
+        }
+    }
+
+    class HabbitMQMessage : Message
+    {
+        public override void SendMessage()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    class AzureServiceBusMessage : Message
+    {
+        public override void SendMessage()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
